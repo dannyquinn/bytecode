b61475736a136c32dcc9feb8b6c854b66ed1a97f
Dan <dannyquinn7@me.com>
2022-05-07 23:03:49 +0100

    Comparison & Booleans

--- clox/value.h
+++ clox/value.h
@@ -28,7 +28,7 @@ typedef struct
 #define AS_NUMBER(value) ((value).as.number)
 
 #define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
-#define NIL_VAL(value) ((Value){VAL_NIL, {.number = 0}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
 typedef struct
@@ -37,6 +37,8 @@ typedef struct
 	int count;
 	Value* values;
 } ValueArray;
+
+bool valuesEqual(Value a, Value b);
 
 void initValueArray(ValueArray* array);
 
