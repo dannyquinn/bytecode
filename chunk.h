2308d5139da76b5da30be4a6bcbcd709207784bf
Danny Quinn <dannyquinn7@me.com>
2024-06-18 15:08:24 +0100

    CH 1

--- chunk.h
+++ chunk.h
@@ -1,34 +1,11 @@
-#ifndef clox_chunk_h 
-#define clox_chunk_h 
+#ifndef clox_chunk_h
+#define clox_chunk_h
 
 #include "common.h"
 #include "value.h"
 
 typedef enum {
     OP_CONSTANT,
-    OP_NIL,
-    OP_TRUE,
-    OP_FALSE,
-    OP_EQUAL,
-    OP_GREATER,
-    OP_LESS,
-    OP_NOT,
-    OP_ADD,
-    OP_SUBTRACT,
-    OP_MULTIPLY,
-    OP_DIVIDE,
-    OP_NEGATE,
-    OP_PRINT,
-    OP_POP,
-    OP_JUMP_IF_FALSE,
-    OP_JUMP,
-    OP_LOOP,
-    OP_CALL,
-    OP_DEFINE_GLOBAL,
-    OP_GET_GLOBAL,
-    OP_SET_GLOBAL,
-    OP_GET_LOCAL,
-    OP_SET_LOCAL,
     OP_RETURN,
 } OpCode;
 
@@ -41,9 +18,11 @@ typedef struct {
 } Chunk;
 
 void initChunk(Chunk* chunk);
-void freeChunk(Chunk* chunk);
+
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
 
 int addConstant(Chunk* chunk, Value value);
 
-#endif
+void freeChunk(Chunk* chunk);
+
+#endif 
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 
 Compile - gcc *.h *.c -o clox.exe 
 
-Run - ./clox.exe test/for.clox 
+Run - ./clox.exe
