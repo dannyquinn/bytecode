41bddb6dda8f8da03f601ebc8613f2035f0c3c9c
Danny Quinn <dannyquinn7@me.com>
2024-06-20 10:59:47 +0100

    CH 3

--- vm.h
+++ vm.h
@@ -16,13 +16,13 @@ typedef struct {
 typedef enum {
     INTERPRET_OK,
     INTERPRET_COMPILE_ERROR,
-    INTERPRET_RUNTINE_ERROR
+    INTERPRET_RUNTIME_ERROR
 } InterpretResult;
 
 void initVM();
 void freeVM();
 
-InterpretResult interpret(Chunk* chunk);
+InterpretResult interpret(const char* source);
 
 void push(Value value);
 Value pop();
